0768ec896d1cfe1dbf08a77939a36349c28b9149
JaviMera <javier_mera88@hotmail.com>
2021-05-10 16:31:53 -0500

    Added database connection string and created database with NationalPark table using migrations

---
+++ NationalParkApi/NationalParkApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NationalParkApi.Models;
+
+namespace NationalParkApi.Data
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public DbSet<NationalPark> NationalParks { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
