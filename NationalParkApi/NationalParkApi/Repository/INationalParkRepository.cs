b9ff8b507496342e203d6b27179915a5c125cf30
JaviMera <javier_mera88@hotmail.com>
2021-05-10 16:41:14 -0500

    Begin adding files for national park repository

---
+++ NationalParkApi/NationalParkApi/Repository/INationalParkRepository.cs
@@ -0,0 +1,16 @@
+﻿using NationalParkApi.Models;
+using System.Collections.Generic;
+
+namespace NationalParkApi.Repository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int nationalParkId);
+        bool Exists(string name);
+        bool Exists(int parkId);
+        bool CreateNationalPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+    }
+}
---
+++ NationalParkApi/NationalParkApi/Repository/NationalParkRepository.cs
@@ -0,0 +1,43 @@
+﻿using NationalParkApi.Models;
+using System.Collections.Generic;
+
+namespace NationalParkApi.Repository
+{
+    public sealed class NationalParkRepository : INationalParkRepository
+    {
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Exists(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Exists(int parkId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
