b47df51cfaa2fdf9eeea23376e0c93288b2d77b3
JaviMera <javier_mera88@hotmail.com>
2021-05-11 08:31:20 -0500

    Begin adding national park dto

---
+++ NationalParkApi/NationalParkApi/Models/Dtos/NationalParkDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NationalParkApi.Models.Dtos
+{
+    public sealed class NationalParkDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
