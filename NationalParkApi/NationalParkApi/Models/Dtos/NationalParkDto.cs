7b5c28a9669f4dfeb67033132a5e8074f65874b6
JaviMera <javier_mera88@hotmail.com>
2021-05-15 18:02:52 -0500

    National Park Dto now requires Name and State props to have a value.

--- NationalParkApi/NationalParkApi/Models/Dtos/NationalParkDto.cs
+++ NationalParkApi/NationalParkApi/Models/Dtos/NationalParkDto.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NationalParkApi.Models.Dtos
 {
     public sealed class NationalParkDto
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string State { get; set; }
+
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
     }
